6247201aaf22c450594113ce6f7dfadc06316220
Chatmechant <63235115+Chatmechant@users.noreply.github.com>
2022-02-01 11:58:46 +0100

    min_max

--- include/algorithme_min_max.hpp
+++ include/algorithme_min_max.hpp
@@ -1,22 +1,37 @@
 #ifndef ALGORITHME_MIN_MAX_HPP
 #define ALGORITHME_MIN_MAX_HPP
 #include <iostream>
+#define inf=1000000
 
 
 class Position 
 {
 public:
-    Plateau plateauRef; // classe Plateau à implémenter
+    Position(Echiquier& plateau, Coup& coups, Position* positionSoeur, Position* positionFille, bool joueurCoup);
+    Echiquier plateauRef; // classe Plateau à implémenter
     Coup* coupsPrecedents; // classe Coup à implémenter
-    int valeurPosition();
+    int valeurPosition = 0; // attribut, à iniialiser avec valeurPosition()
     Position* soeur;
     Position* fille;
     bool joueur;
+
+    bool estGagnante();
+    int valeurPosition();
     void generateur(int profondeur);
+    ~Position();
 };
 
-class Plateau {
-
+class Coup
+{
+public:
+    Coup(bool isW, Piece& pieceJ, pair<int,int>& newP, pair<int,int>& oldP, bool isTaken=False, bool isSpecial=False);
+    bool isWhite;
+    Piece pieceJouee;
+    pair<int,int> oldPosition;
+    pair<int,int> newPosition;
+    bool isTaken;
+    bool isSpecial;
 };
+
 
 #endif
